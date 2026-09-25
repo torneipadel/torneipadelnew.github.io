1cbb14ae8a309132d8a6092ae9e783d648aa45b5
josriz <giose.rizzi@gmail.com>
2026-09-25 15:41:24 +0200

    Migra client Supabase alla publishable key MASTER

--- visitatore-eventi-v1.js
+++ visitatore-eventi-v1.js
@@ -6,7 +6,7 @@
 'use strict';
 
 const SUPABASE_URL='https://dkeqicstprvvfebiaooc.supabase.co';
-const SUPABASE_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrZXFpY3N0cHJ2dmZiaWFvY29jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNjQ2NjksImV4cCI6MjA3Njc0MDY2OX0.MPlE8CZ2B8pEhSJzTWBJ-FfFLQpwRAxlAlno-SCylWg';
+const SUPABASE_KEY='sb_publishable_EBgrU25BpXMp9x6a2n7_Pg_FTFa5JLu';
 
 let sb=null;
 let allTournaments=[];
